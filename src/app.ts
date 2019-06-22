0d8460966c1fa5f5b41e464767e6a7aea889e106
LJC0915 <bxh.red@gmail.com>
2019-06-22 19:26:07 +0800

    add travis CI

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
